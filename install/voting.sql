43b5ab37af82fa215349cfbd137cdf607c843e7e
remcodezwart <remcodezwart1997@gmail.com>
2017-05-07 22:51:07 +0200

    updated sql and added er diagram

--- install/voting.sql
+++ install/voting.sql
@@ -1,9 +1,9 @@
--- phpMyAdmin SQL Dump
+﻿-- phpMyAdmin SQL Dump
 -- version 4.6.4
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Gegenereerd op: 07 mei 2017 om 16:00
+-- Gegenereerd op: 07 mei 2017 om 20:49
 -- Serverversie: 5.7.14
 -- PHP-versie: 5.6.25
 
@@ -72,8 +72,19 @@ INSERT INTO `parties` (`id`, `name`, `active`) VALUES
 
 CREATE TABLE `statements` (
   `id` int(11) NOT NULL,
-  `description` varchar(255) NOT NULL
+  `description` varchar(255) NOT NULL,
+  `active` int(11) NOT NULL DEFAULT '1'
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Gegevens worden geëxporteerd voor tabel `statements`
+--
+
+INSERT INTO `statements` (`id`, `description`, `active`) VALUES
+(1, 'De rijken moeten meer belasting betalen', 1),
+(2, 'Er moet meer geld naar het mileu', 1),
+(3, 'test', 1),
+(4, 'het leenstelsel moet weg', 1);
 
 -- --------------------------------------------------------
 
@@ -90,7 +101,6 @@ CREATE TABLE `users` (
   `user_active` tinyint(1) NOT NULL DEFAULT '0' COMMENT 'user''s activation status',
   `user_deleted` tinyint(1) NOT NULL DEFAULT '0' COMMENT 'user''s deletion status',
   `user_account_type` tinyint(1) NOT NULL DEFAULT '1' COMMENT 'user''s account type (basic, premium, etc)',
-  `user_has_avatar` tinyint(1) NOT NULL DEFAULT '0' COMMENT '1 if user has a local avatar, 0 if not',
   `user_remember_me_token` varchar(64) COLLATE utf8_unicode_ci DEFAULT NULL COMMENT 'user''s remember-me cookie token',
   `user_creation_timestamp` bigint(20) DEFAULT NULL COMMENT 'timestamp of the creation of user''s account',
   `user_suspension_timestamp` bigint(20) DEFAULT NULL COMMENT 'Timestamp till the end of a user suspension',
@@ -107,9 +117,9 @@ CREATE TABLE `users` (
 -- Gegevens worden geëxporteerd voor tabel `users`
 --
 
-INSERT INTO `users` (`user_id`, `session_id`, `user_name`, `user_password_hash`, `user_email`, `user_active`, `user_deleted`, `user_account_type`, `user_has_avatar`, `user_remember_me_token`, `user_creation_timestamp`, `user_suspension_timestamp`, `user_last_login_timestamp`, `user_failed_logins`, `user_last_failed_login`, `user_activation_hash`, `user_password_reset_hash`, `user_password_reset_timestamp`, `user_provider_type`) VALUES
-(1, NULL, 'demo', '$2y$10$OvprunjvKOOhM1h9bzMPs.vuwGIsOqZbw88rzSyGCTJTcE61g5WXi', 'demo@demo.com', 1, 0, 7, 0, NULL, 1422205178, NULL, 1422209189, 0, NULL, NULL, NULL, NULL, 'DEFAULT'),
-(2, '3e8u8vdm3v025n90n1adoq4ul5', 'demo2', '$2y$10$OvprunjvKOOhM1h9bzMPs.vuwGIsOqZbw88rzSyGCTJTcE61g5WXi', 'demo2@demo.com', 1, 0, 7, 0, NULL, 1422205178, NULL, 1494079639, 0, NULL, NULL, NULL, NULL, 'DEFAULT');
+INSERT INTO `users` (`user_id`, `session_id`, `user_name`, `user_password_hash`, `user_email`, `user_active`, `user_deleted`, `user_account_type`, `user_remember_me_token`, `user_creation_timestamp`, `user_suspension_timestamp`, `user_last_login_timestamp`, `user_failed_logins`, `user_last_failed_login`, `user_activation_hash`, `user_password_reset_hash`, `user_password_reset_timestamp`, `user_provider_type`) VALUES
+(1, NULL, 'demo', '$2y$10$OvprunjvKOOhM1h9bzMPs.vuwGIsOqZbw88rzSyGCTJTcE61g5WXi', 'demo@demo.com', 1, 0, 7, NULL, 1422205178, NULL, 1422209189, 0, NULL, NULL, NULL, NULL, 'DEFAULT'),
+(2, '90j6chfiijhuha3929r4j4l773', 'demo2', '$2y$10$OvprunjvKOOhM1h9bzMPs.vuwGIsOqZbw88rzSyGCTJTcE61g5WXi', 'demo2@demo.com', 1, 0, 7, NULL, 1422205178, NULL, 1494178363, 0, NULL, NULL, NULL, NULL, 'DEFAULT');
 
 -- --------------------------------------------------------
 
@@ -193,7 +203,7 @@ ALTER TABLE `parties`
 -- AUTO_INCREMENT voor een tabel `statements`
 --
 ALTER TABLE `statements`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 --
 -- AUTO_INCREMENT voor een tabel `users`
 --
